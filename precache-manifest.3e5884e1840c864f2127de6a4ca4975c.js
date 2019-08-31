2214d5841e8b54cbcdcdce177301955cef29b98f
YousraKhan <yousra.khan97@hotmail.com>
2019-09-01 02:32:28 +0500

    Updates

---
+++ precache-manifest.3e5884e1840c864f2127de6a4ca4975c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c558a8a40aacfb82421cdb772ead8628",
+    "url": "/mysecondcalculator/index.html"
+  },
+  {
+    "revision": "3258de8e7b9331a202d2",
+    "url": "/mysecondcalculator/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "db657bb11b8c6311feac",
+    "url": "/mysecondcalculator/static/css/main.305a4a9b.chunk.css"
+  },
+  {
+    "revision": "3258de8e7b9331a202d2",
+    "url": "/mysecondcalculator/static/js/2.a22b0249.chunk.js"
+  },
+  {
+    "revision": "db657bb11b8c6311feac",
+    "url": "/mysecondcalculator/static/js/main.6aaf3af8.chunk.js"
+  },
+  {
+    "revision": "7f23e8f586d3edbb6242",
+    "url": "/mysecondcalculator/static/js/runtime~main.346a51ef.js"
+  }
+]);
